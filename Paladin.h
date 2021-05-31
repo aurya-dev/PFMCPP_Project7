1365addd799f9e09595bbc49265b8c53bf0c9f26
AURYA <>
2021-05-31 11:48:09 +0000

    = 0 to { }

--- Paladin.h
+++ Paladin.h
@@ -5,7 +5,7 @@
 struct Paladin : Character
 {
     Paladin(const std::string name_, int hp, int armor_, int attackDamage_ = 0) : Character(hp, armor_, attackDamage_ ), name(name_) {}
-    ~Paladin() override {}
+    ~Paladin() override { }
     const std::string& getName() override;
     std::string getStats() override;
 private:
